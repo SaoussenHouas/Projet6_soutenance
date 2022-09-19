66b5bfa8822fa3bfe2f6d71cab1d6805c2b67c58
Saoussen HOUAS <kouki.saoussen94@gmail.com>
2022-09-19 03:01:50 +0200

    third commit

---
+++ backend/middleware/auth.js
@@ -0,0 +1,21 @@
+//Module
+const jwt = require("jsonwebtoken");
+
+//Token verification
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.RND_TKN);
+    const userId = decodedToken.userId;
+    req.auth = {
+      userId: userId,
+    };
+    if (req.body.userId && req.body.userId !== userId) {
+      throw 'User ID non valable !'
+    } else {
+        next()
+    } 
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+};
